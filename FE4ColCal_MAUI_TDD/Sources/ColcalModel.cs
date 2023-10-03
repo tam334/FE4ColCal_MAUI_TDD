fa8a558433092d66c14c33cb7d2bf2690919ea21
y_tsukuda <y.tsukuda.gm.01@gmail.com>
2023-10-03 17:59:51 +0900

    とりあえず確率計算用のAPIを定義

---
+++ FE4ColCal_MAUI_TDD/Sources/ColcalModel.cs
@@ -0,0 +1,49 @@
+﻿using System;
+namespace FE4ColCal_MAUI_TDD
+{
+	public class ColCalModel
+	{
+		public const int ARENA_FIELD_EFFECT = 20;
+
+		/// <summary>
+		/// パラメータ格納クラス
+		/// </summary>
+		public class Parameter
+		{
+			public int hp;
+			public int hit;
+            public int atc;
+			public int def;
+			public int aspd;
+			public bool chase;
+			public bool datk;
+			public int crit;
+			public int shield;
+        }
+
+		/// <summary>
+		/// 勝率の計算
+		/// </summary>
+		/// <param name="player">プレイヤーのパラメータ</param>
+		/// <param name="enemy">敵のパラメータ</param>
+		/// <returns>勝率</returns>
+		/// <exception cref="NotImplementedException"></exception>
+		public float Calc(Parameter player, Parameter enemy)
+		{
+			throw new NotImplementedException();
+		}
+
+		/// <summary>
+		/// 敵のデータのロード
+		/// </summary>
+		/// <param name="chapter">章</param>
+		/// <param name="level">闘技レベル</param>
+		/// <returns>パラメータ</returns>
+		/// <exception cref="NotImplementedException"></exception>
+		public Parameter LoadEnemyParam(int chapter, int level)
+		{
+			throw new NotImplementedException();
+		}
+	}
+}
+
