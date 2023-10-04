010ffb84cbdb39672c0aaab170ef4a353afc737b
y_tsukuda <y.tsukuda.gm.01@gmail.com>
2023-10-04 14:13:34 +0900

    相手が先手を取る際の失敗ケースを作る

--- FE4ColCal_Test/UnitTest1.cs
+++ FE4ColCal_Test/UnitTest1.cs
@@ -8,7 +8,7 @@ public class UnitTest1
     {
         ColCalModel model = new ColCalModel();
 
-        //一撃でプレイヤーが勝利する
+        //プレイヤーが先手を取り、一撃で勝利する
         Assert.InRange(model.Calc(
             new ColCalModel.Parameter()
             {
@@ -37,5 +37,35 @@ public class UnitTest1
             ),
             0.99999f,
             1.0f);
+
+        //相手が先手を取り、一撃で勝利する
+        Assert.InRange(model.Calc(
+            new ColCalModel.Parameter()
+            {
+                hp = 1,
+                hit = 100,
+                atc = 2,
+                def = 1,
+                aspd = 1,
+                chase = false,
+                datk = false,
+                crit = 0,
+                shield = 0
+            },
+            new ColCalModel.Parameter()
+            {
+                hp = 1,
+                hit = 100,
+                atc = 2,
+                def = 1,
+                aspd = 2,
+                chase = false,
+                datk = false,
+                crit = 0,
+                shield = 0
+            }
+            ),
+            0.0f,
+            0.0000001f);
     }
 }
